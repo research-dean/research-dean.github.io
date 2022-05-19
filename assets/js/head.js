548ac6e644886fd873d0c6a47c518e743096cf58
research-dean <gentlehjx@hotmail.com>
2022-05-19 14:49:55 +0800

    third live

--- assets/js/head.js
+++ assets/js/head.js
@@ -4,8 +4,23 @@ $(document).ready(function() {
     if (path === '/') {
         $('#nav-home').addClass('active');
     } else if (path == '/about.html') {
-        $('#nav-about').addClass('active');
+        var hash = window.location.hash
+        if (hash === '') {
+            $('#nav-about').addClass('active');
+        } else if (hash === '#sw') {
+            $('#nav-sw').addClass('active');
+        } else if (hash === '#contact') {
+            $('#nav-contact').addClass('active');
+        }
+        
     } else if (path === '/blog.html') {
         $('#nav-blog').addClass('active');
+    } else if (path === '/team.html') {
+        $('#nav-team').addClass('active');
     }
+
+    $(".nav-li").on("click", function() {
+        $( "#nav-ul .nav-li" ).removeClass("active");
+        $(this).addClass('active');
+    })
 });
