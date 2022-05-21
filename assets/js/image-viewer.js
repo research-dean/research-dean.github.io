5f411416955d6353d90a401e2b56e0b4b6a44b1b
research-dean <gentlehjx@hotmail.com>
2022-05-21 15:41:24 +0800

    4th live, add record images

---
+++ assets/js/image-viewer.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 2,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+console.log("fuck");
+console.log(swiper);
